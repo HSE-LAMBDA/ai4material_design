7d14171a713a2822f467bf132d4c3f358679f7f3
Nikita Kazeev <nikita.kazeev@cern.ch>
2021-08-04 23:50:57 +0000

    Added dz as edge feature refactoring

--- defect_representation.py
+++ defect_representation.py
@@ -1,10 +1,104 @@
+from copy import deepcopy
+from typing import List, Dict, Union
+import numpy as np
+
 from pymatgen.core.periodic_table import DummySpecies
+from pymatgen.core import Structure
+from pymatgen.analysis.local_env import NearNeighbors
+
 from megnet.data.crystal import CrystalGraph
+from megnet.data.graph import Converter
+from megnet.data.graph import GaussianDistance
+
+
+class FlattenGaussianDistance(GaussianDistance):
+    def convert(self, d: np.ndarray) -> np.ndarray:
+        """
+        Presumes that the bond features are different distances, e. g.
+        the total distances and distance in z axis produced by
+        VacancyAwareStructureGraph with add_bond_z_coord.
+
+        Transforms each distance component with 
+        megnet.data.graph.GaussianDistance and concatenates the
+        resulting vectors to serve as edge features.
+        """
+        if len(d.shape) != 2:
+            return ValueError("Input array must be 2-dimensional")
+        return np.concatenate(
+            [super(FlattenGaussianDistance, self).convert(arr) for arr in d.T],
+            axis=1)
 
 
 class VacancyAwareStructureGraph(CrystalGraph):
-    n_atom_features = 2
+    def __init__(self,
+                 nn_strategy: Union[str, NearNeighbors] = "MinimumDistanceNNAll",
+                 atom_converter: Converter = None,
+                 bond_converter: Converter = None,
+                 cutoff: float = 5.0,
+                 add_displaced_species: bool = True,
+                 add_bond_z_coord: bool = True):
+        """"
+        Args:
+         Same as CrystalGraph:
+           nn_strategy (str): NearNeighbor strategy
+           atom_converter (Converter): atom features converter
+           bond_converter (Converter): bond features converter
+           cutoff (float): cutoff radius
+         Added:
+           add_displaced_species (bool): if set, add the species that
+               occupied the defects sites in the pristine material
+           add_bond_z_coord: if set, add abs(atom_1.z_coord - atom_2.z_coord) as a bond feature.
+                Useful, as in our 2D materials z axis is special
+        """
+        super().__init__(
+            nn_strategy=nn_strategy,
+            atom_converter=atom_converter,
+            bond_converter=bond_converter,
+            cutoff=cutoff)
+
+        self.add_displaced_species = add_displaced_species
+        if add_displaced_species:
+            self.nfeat_node = 2
+        else:
+            self.nfeat_node = 1
+        
+        self.add_bond_z_coord = add_bond_z_coord
+        if add_bond_z_coord:
+            self.nfeat_edge = 2
+        else:
+            self.nfeat_edge = 1
+
+    def get_atom_features(self, structure):
+        if self.add_displaced_species:
+            return [[0 if isinstance(i.specie, DummySpecies) else i.specie.Z,
+                     i.properties['was']] for i in structure.sites]
+        else:
+            return [[0 if isinstance(i, DummySpecies) else i.Z]
+                    for i in structure.species]
+
+    def convert(self, structure: Structure) -> Dict:
+        """
+        Convert structure into graph
+        Args:
+            structure (Structure): pymatgen Structure
+        Returns: graph dictionary
+        """
+        graph = super().convert(structure)
+        if self.add_bond_z_coord:
+            return self._add_bond_z_coord(graph, structure.cart_coords[:, 2])
+        else:
+            return graph
+
     @staticmethod
-    def get_atom_features(structure):
-        return [[0 if isinstance(i.specie, DummySpecies) else i.specie.Z,
-                 i.properties['was']] for i in structure.sites]
+    def _add_bond_z_coord(graph, z_coords) -> Dict:
+        """
+        For 2D materials in our project, z-axis is a special axis, as it is orthogonal
+        to the material surface. Thus, we include the information on delta z to 
+        the edge features.
+        """
+        new_graph = deepcopy(graph)
+        new_graph["bond"] = np.empty((graph["bond"].shape[0], 2), graph["bond"].dtype)
+        new_graph["bond"][:, 0] = graph["bond"]
+        for k, (i, j) in enumerate(zip(graph["index1"], graph["index2"])):
+            new_graph["bond"][k] = abs(z_coords[i] - z_coords[j])
+        return new_graph
--- megnet_graphs_train.py
+++ megnet_graphs_train.py
@@ -1,14 +1,17 @@
 import os
+import itertools
+from operator import methodcaller
 import numpy as np
 import pandas as pd
 import argparse
+from multiprocessing import Pool
 from pynvml import nvmlInit, nvmlDeviceGetHandleByIndex, nvmlDeviceGetMemoryInfo, nvmlDeviceGetCount
 
 from megnet.models import MEGNetModel
 from megnet.utils.preprocessing import StandardScaler
 from megnet.data.graph import GaussianDistance
 
-from defect_representation import VacancyAwareStructureGraph
+from defect_representation import VacancyAwareStructureGraph, FlattenGaussianDistance
 
 
 def get_free_gpu():
@@ -19,37 +22,96 @@ def get_free_gpu():
     ])
 
 
-def main():
-    # TODO(kazeevn) elegant device configration
-    os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = "true"
-    os.environ['CUDA_VISIBLE_DEVICES'] = str(get_free_gpu())
-    parser = argparse.ArgumentParser(description='Train MEGNet on defect graph.')
-    parser.add_argument('--train', type=str, required=True, help='pickled train to use')
-    parser.add_argument('--target', type=str, required=True, help='target to predict')
-    parser.add_argument('--is-intensive', type=bool, required=True)
-    parser.add_argument('--experiment-name', type=str, default=None)
-    parser.add_argument('--epochs', type=int, default=1000)
-    args = parser.parse_args()
-    train = pd.read_pickle(args.train)
+IS_INTENSIVE = {
+    "homo": True,
+    "formation_energy": False
+}
 
-    nfeat_edge = 10
-    gc = VacancyAwareStructureGraph(
-        bond_converter=GaussianDistance(np.linspace(0, 15, nfeat_edge), 0.5),
-        cutoff=15)
-    model = MEGNetModel(nfeat_edge=nfeat_edge,
-                        nfeat_node=gc.n_atom_features,
-                        nfeat_global=2,
-                        graph_converter=gc,
-                        npass=2)
-    scaler = StandardScaler.from_training_data(train.defect_representation,
-                                               train[args.target],
-                                               is_intensive=args.is_intensive)
-    model.target_scaler = scaler
-    model.train(train.defect_representation,
-                train[args.target],
-                epochs=args.epochs,
-                verbose=1)
-    model.save_model(os.path.join("models", "MEGNet-defect-only", args.target, args.experiment_name))
+TRAIN_CORE_PATH = r"datasets/train_defects{}.pickle.gzip"
+MODEL_PATH_ROOT = os.path.join("models", "MEGNet-defect-only")
+class Experiment():
+    def __init__(self,
+                 target: str,
+                 vacancy_only: bool,
+                 add_displaced_species: bool,
+                 add_bond_z_coord: bool,
+                 epochs: int = 1000,
+                 ):
+        if vacancy_only:
+            self.train_path = TRAIN_CORE_PATH.format("_vac_only")
+        else:
+            self.train_path = TRAIN_CORE_PATH.format("")
+        self.name = (f"{'vac_only' if vacancy_only else 'full'}"
+                     f"{'_bond_z' if add_bond_z_coord else ''}"
+                     f"{'_werespecies' if add_displaced_species else ''}")
+        self.target = target
+        self.epochs = epochs
+        self.add_displaced_species = add_displaced_species
+        self.add_bond_z_coord = add_bond_z_coord
+        self.model_path = os.path.join(MODEL_PATH_ROOT, self.target, self.name)
+        # This parameter is not used in run(), but is saved for reference
+        self.vacancy_only = vacancy_only
+
+    def run(self):
+        train = pd.read_pickle(self.train_path)
+        nfeat_edge_per_dim = 5
+        cutoff = 15
+        if self.add_bond_z_coord:
+            bond_converter = FlattenGaussianDistance(
+                np.linspace(0, cutoff, nfeat_edge_per_dim), 0.5)
+        else:
+            bond_converter = GaussianDistance(
+                np.linspace(0, cutoff, nfeat_edge_per_dim), 0.5)
+        graph_converter = VacancyAwareStructureGraph(
+            bond_converter=bond_converter,
+            add_displaced_species=self.add_displaced_species,
+            add_bond_z_coord=self.add_bond_z_coord,
+            cutoff=cutoff)
+        scaler = StandardScaler.from_training_data(train.defect_representation,
+                                                   train[self.target],
+                                                   is_intensive=IS_INTENSIVE[self.target])
+        # TODO(kazeevn) elegant device configration
+        os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = "true"
+        os.environ['CUDA_VISIBLE_DEVICES'] = str(get_free_gpu())
+        # TODO(kazeevn) consider embeddings for the atomic numbers
+        model = MEGNetModel(nfeat_edge=graph_converter.nfeat_edge*nfeat_edge_per_dim,
+                            nfeat_node=graph_converter.nfeat_node,
+                            nfeat_global=2,
+                            graph_converter=graph_converter,
+                            npass=2,
+                            target_scaler=scaler)
+        model.train(train.defect_representation,
+                    train[self.target],
+                    epochs=self.epochs,
+                    verbose=1)
+        model.save_model(self.model_path)
+
+
+# https://stackoverflow.com/questions/5228158/cartesian-product-of-a-dictionary-of-lists
+def product_dict(**kwargs):
+    keys = kwargs.keys()
+    vals = kwargs.values()
+    for instance in itertools.product(*vals):
+        yield dict(zip(keys, instance))
+
+
+TARGETS = ("homo", "formation_energy")
+def generate_expetiments():
+    param_values = {
+        "target": TARGETS,
+        "vacancy_only": (True, False),
+        "add_displaced_species": (True, False),
+        "add_bond_z_coord": (True, False),
+        "epochs": [1000],
+    }    
+    return [Experiment(**params) for params in product_dict(**param_values)]
+
+
+def main():
+    experiments = generate_expetiments()
+    # We are light on GPU usage
+    with Pool(8) as p:
+        p.map(methodcaller("run"), experiments)
 
     
 if __name__ == '__main__':
