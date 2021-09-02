dc6d35db864b9a16c6de8fa30643a8b94d1b99b5
Nikita Kazeev <nikita.kazeev@cern.ch>
2021-09-02 23:30:31 +0000

    Supercell replication and band gap experiments

--- data.py
+++ data.py
@@ -20,22 +20,32 @@ def get_gpaw_trajectories(defect_db_path:str):
     return res
 
 
+def read_structures_descriptions(data_path:str):
+    return pd.read_csv(os.path.join(data_path, "defects.csv"),
+                       index_col="_id",
+                       # An explicit list of columns is due to the fact that
+                       # dichalcogenides8x8_innopolis_202108/defects.csv
+                       # contains an unnamed index column, and
+                       # datasets/dichalcogenides_innopolis_202105/defects.csv
+                       # doesn't
+                       usecols=["_id",
+                                "descriptor_id",
+                                "defect_id",
+                                "energy",
+                                "energy_per_atom",
+                                "fermi_level",
+                                "homo",
+                                "lumo"])
+
+
+def read_defects_descriptions(data_path:str):
+    return pd.read_csv(
+        os.path.join(data_path, "descriptors.csv"), index_col="_id",
+        converters={"cell": eval, "defects": eval})    
+
+
 def get_dichalcogenides_innopolis(data_path:str):
-    structures = pd.read_csv(os.path.join(data_path, "defects.csv"),
-                             index_col="_id",
-                             # An explicit list of columns is due to the fact that
-                             # dichalcogenides8x8_innopolis_202108/defects.csv
-                             # contains an unnamed index column, and
-                             # datasets/dichalcogenides_innopolis_202105/defects.csv
-                             # doesn't
-                             usecols=["_id",
-                                      "descriptor_id",
-                                      "defect_id",
-                                      "energy",
-                                      "energy_per_atom",
-                                      "fermi_level",
-                                      "homo",
-                                      "lumo"])
+    structures = read_structures_descriptions(data_path)
     initial_structures = dict()
     structures_folder = os.path.join(data_path, "initial")
     for structure_file in tqdm(os.listdir(structures_folder)):
@@ -43,4 +53,4 @@ def get_dichalcogenides_innopolis(data_path:str):
         initial_structures[os.path.splitext(structure_file)[0]] = \
             this_file.get_structures(primitive=False)[0]
     structures["initial_structure"] = structures.apply(lambda row: initial_structures[row.name], axis=1)
-    return structures
+    return structures, read_defects_descriptions(data_path)
