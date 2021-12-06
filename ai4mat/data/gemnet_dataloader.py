7a79d55cf1991717b949f43f43e6153bd85358ec
Abdalaziz <abdalaziz.rashid@outlook.com>
2021-12-06 15:37:24 +0300

    Add: dataloader

--- ai4mat/data/gemnet_dataloader.py
+++ ai4mat/data/gemnet_dataloader.py
@@ -6,8 +6,80 @@ from torch_geometric.data import Batch
 
 from ai4mat.common.atom_to_graph import AtomsToGraphs
 from ai4mat.common.utils import cache
+from functools import lru_cache
 
+class GemNetFullStructFolds:
+    def __init__(self, 
+            train_structures,
+            train_targets,
+            test_structures,
+            test_targets,
+            configs,
+            graph_construction_config=None,
+            ):
 
+        self.graph_construction_config = graph_construction_config
+        self.config = configs
+        
+
+        self.train_graph_list = self.prepare(
+            self.get_ase_atoms(train_structures),
+            train_targets,
+            )
+
+        self.test_graph_list = self.prepare(
+            self.get_ase_atoms(test_structures),
+            test_targets
+        )
+
+    def get_ase_atoms(self, x):
+        return list(map(AseAtomsAdaptor.get_atoms, x))
+    
+    # @cache(name="graph_list_cache")
+    def prepare(self, atoms, targets):
+        a2g = AtomsToGraphs(**self.graph_construction_config)
+        return a2g.convert_all(atoms, metadata_collection=targets.to_frame())
+
+    def _data_list_collater(self, data_list):
+        batch = Batch.from_data_list(data_list)
+        n_neighbors = []
+        for i, data in enumerate(data_list):
+            n_index = data.edge_index[1, :]
+            n_neighbors.append(n_index.shape[0])
+        batch.neighbors = torch.tensor(n_neighbors)
+        return batch
+
+    @property
+    def trainloader(self):
+        return DataLoader(
+            self.train_graph_list,
+            batch_size=self.config["optim"]["batch_size"],
+            collate_fn=self._data_list_collater,
+            num_workers=0,#self.config["optim"]["num_workers"],
+            pin_memory=True,
+            shuffle=True,
+        )
+
+    @property
+    def validloader(self):
+        return DataLoader(
+            self.test_graph_list,
+            batch_size=self.config["optim"]["batch_size"],
+            collate_fn=self._data_list_collater,
+            num_workers=0, #self.config["optim"]["num_workers"],
+            pin_memory=True,
+            shuffle=False,
+        )
+    
+    def testloader(self, data):
+        return DataLoader(
+            data,
+            batch_size=self.config["optim"]["batch_size"],
+            collate_fn=self._data_list_collater,
+            num_workers=0, #self.config["optim"]["num_workers"],
+            pin_memory=True,
+            shuffle=False,
+        )
 class GemNetFullStruct:
     def __init__(self, config):
         self.config = config
