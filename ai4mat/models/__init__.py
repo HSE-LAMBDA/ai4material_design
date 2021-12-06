925f9f5c51df6a8db2b7923c0908498f8ed6cbe1
Abdalaziz <abdalaziz.rashid@outlook.com>
2021-12-06 19:18:56 +0300

    Add: gemnet wrapper

--- ai4mat/models/__init__.py
+++ ai4mat/models/__init__.py
@@ -1,10 +1,10 @@
 from ai4mat.models.megnet_wrapper import get_megnet_predictions
-# from ai4mat.models.gemnet_wrapper import get_gemnet_predictions
+from ai4mat.models.gemnet_wrapper import get_gemnet_predictions
 
 def get_predictor_by_name(name):
     if name == "megnet":
         return get_megnet_predictions
-    # if name == "gemnet":
-        # return get_gemnet_predictions
+    if name == "gemnet":
+        return get_gemnet_predictions
     else:
         raise ValueError("Unknown model name")
