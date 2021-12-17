3fbe3b809b29a2f5c0960b4f2ea13bbcc3a8697f
Abdalaziz <abdalaziz.rashid@outlook.com>
2021-12-17 16:53:37 +0300

    Fix: lumo case

--- ai4mat/data/data.py
+++ ai4mat/data/data.py
@@ -74,7 +74,7 @@ def copy_indexed_structures(structures, input_folder, output_folder):
 
 IS_INTENSIVE = {
     "homo": True,
-    "LUMO": True,
+    "lumo": True,
     "formation_energy": False,
     "band_gap": True,
     "formation_energy_per_site": True
