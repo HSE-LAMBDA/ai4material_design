6d0b3f4f4ce50894219dcc06a8ae4f2588462707
Nikita Kazeev <kna@nus.edu.sg>
2022-05-26 16:04:13 +0000

    Fixed WanDB config

--- run_experiments.py
+++ run_experiments.py
@@ -206,11 +206,9 @@ def predict_on_fold(
     this_wandb_config["test_fold"] = test_fold
 
     with wandb.init(
-        project="ai4material_design_final_run",
+        project="ai4material_design",
         entity=os.environ["WANDB_ENTITY"],
         config=this_wandb_config,
-        name="full-eos",
-        tags=['full', 'eos']
     ) as run:
         return predict_func(
             train,
