542e5c556ef106c78924384c1ef41ad04dc65183
Nikita Kazeev <kna@nus.edu.sg>
2022-06-30 18:34:18 +0000

    cell size 6_8/8 predictions

--- scripts/summary_table.py
+++ scripts/summary_table.py
@@ -11,17 +11,9 @@ sys.path.append('.')
 from ai4mat.data.data import StorageResolver, get_prediction_path, get_targets_path
 
 
-def main():
-    parser = argparse.ArgumentParser("Makes a text experiment/trial table")
-    parser.add_argument("--experiment", type=str, required=True)
-    parser.add_argument("--trials", type=str, nargs="+")
-    parser.add_argument("--unit-multiplier", type=float, default=1e3,
-        help="As of May 2022, data in the project are in eV, so the 1000 mutliplier makes it meV")
-    args = parser.parse_args()
-    
+def print_target_trial_table(experiment, trials, unit_multiplier):
     storage_resolver = StorageResolver()
-
-    experiment_path = storage_resolver["experiments"].joinpath(args.experiment)
+    experiment_path = storage_resolver["experiments"].joinpath(experiment)
     with open(experiment_path.joinpath("config.yaml")) as experiment_file:
         experiment = yaml.safe_load(experiment_file)
     folds = pd.read_csv(experiment_path.joinpath("folds.csv"),
@@ -46,10 +38,67 @@ def main():
             errors = np.abs(predictions - true_targets.loc[:, target_name])
             mae = errors.mean()
             mae_cv_std = np.std(errors.groupby(by=folds).mean())
-            row.append(f"{mae*args.unit_multiplier:.1f} ± {mae_cv_std*args.unit_multiplier:.1f}")
+            row.append(f"{mae*unit_multiplier:.1f} ± {mae_cv_std*unit_multiplier:.1f}")
         mae_table.add_row(row)
     print(mae_table)
-            
+
+
+def print_experiment_trial_table(experiments, trials, target_name, unit_multiplier):
+    storage_resolver = StorageResolver()
+    mae_table = pt()
+    mae_table.field_names = ["Experiment"] + trials
+    for experiment_name in experiments:
+        row = [experiment_name]
+        experiment_path = storage_resolver["experiments"].joinpath(experiment_name)
+        with open(experiment_path.joinpath("config.yaml")) as experiment_file:
+            experiment = yaml.safe_load(experiment_file)
+        folds = pd.read_csv(experiment_path.joinpath("folds.csv"),
+                            index_col="_id",
+                            squeeze=True)
+        true_targets = pd.concat([pd.read_csv(get_targets_path(path), index_col="_id",
+                                    usecols=["_id", target_name]).squeeze("columns")
+                                    for path in experiment["datasets"]], axis=0).reindex(
+                                    index=folds.index)
+        for trial in trials:
+            predictions = pd.read_csv(storage_resolver["predictions"].joinpath(
+                                            get_prediction_path(
+                                                experiment_name,
+                                                target_name,
+                                                trial
+                                            )), index_col="_id", squeeze=True)
+            these_targets = true_targets.reindex(index=predictions.index)
+            errors = np.abs(predictions - these_targets)
+            mae = errors.mean()
+            row.append(f"{mae*unit_multiplier:.1f}")
+        mae_table.add_row(row)
+    print(mae_table)
+
+
+def main():
+    parser = argparse.ArgumentParser("Makes a text table with MAEs")
+    parser.add_argument("--experiments", type=str, nargs="+")
+    parser.add_argument("--trials", type=str, nargs="+")
+    parser.add_argument("--separate-by", choices=["experiment", "target"],
+        help="Tables are 2D, but we have 3 dimensions: target, trial, experiment. "
+        "One of them must be used to separate the tables.")
+    parser.add_argument("--unit-multiplier", type=float, default=1e3,
+        help="As of May 2022, data in the project are in eV, so the 1000 mutliplier makes it meV")
+    args = parser.parse_args()
+    
+    if args.separate_by == "experiment":
+        for experiment in args.experiments:
+            print_target_trial_table(experiment, args.trials, args.unit_multiplier)
+    elif args.separate_by == "target":
+        storage_resolver = StorageResolver()
+        experiment_path = storage_resolver["experiments"].joinpath(args.experiments[0])
+        with open(experiment_path.joinpath("config.yaml")) as experiment_file:
+            experiment = yaml.safe_load(experiment_file)
+        targets = experiment["targets"]
+        for target_name in targets:
+            print(f"{target_name}:")
+            print_experiment_trial_table(
+                args.experiments, args.trials, target_name, args.unit_multiplier)
+   
 
 if __name__ == "__main__":
     main()
