72aa5cb73ee3b308e9b967e9b4cb29cb1b5eecd9
Nikita Kazeev <kazeevn@gmail.com>
2022-08-15 17:10:36 +0800

    Plotting parameters

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,3 +1,4 @@
+from ast import parse
 from itertools import product
 import argparse
 from pathlib import Path
@@ -15,6 +16,10 @@ def main():
     parser = argparse.ArgumentParser("Plots predictions")
     parser.add_argument("--experiments", type=str, nargs="+")
     parser.add_argument("--trials", type=str, nargs="+")
+    parser.add_argument("--targets", type=str, nargs="+")
+    parser.add_argument("--filetype", type=str, default="pdf")
+    parser.add_argument("--units", type=str, default="eV")
+    parser.add_argument("--limits", type=float, nargs=2)
     args = parser.parse_args()
     
     font = {
@@ -41,7 +46,11 @@ def main():
         true_targets = pd.concat([pd.read_csv(storage_resolver["processed"]/dataset/"targets.csv.gz", index_col="_id")
                                   for dataset in experiment["datasets"]], axis=0).reindex(
                                           index=folds.index)
-        for target_name, this_trial_name in product(experiment["targets"], args.trials):
+        if args.targets:
+            targets = args.targets
+        else:
+            targets = experiment["targets"]
+        for target_name, this_trial_name in product(targets, args.trials):
             predictions = pd.read_csv(storage_resolver["predictions"].joinpath(
                                            get_prediction_path(
                                                experiment_name,
@@ -55,14 +64,16 @@ def main():
                        predictions,
                        label=f"$\mathrm{{MAE}}={mae:.4f}$",
                        **plot_kwargs)
-            ax.set_xlabel(f"DFT {target_name}, eV")
-            ax.set_ylabel(f"Predicted {target_name}, eV")
+            ax.set_xlabel(f"DFT {target_name}, {args.units}")
+            ax.set_ylabel(f"Predicted {target_name}, {args.units}")
             ax.legend()
-
-            lims = [
-                np.min([ax.get_xlim(), ax.get_ylim()]),
-                np.max([ax.get_xlim(), ax.get_ylim()]),
-            ]
+            if args.limits:
+                lims = args.limits
+            else:
+                lims = [
+                    np.min([ax.get_xlim(), ax.get_ylim()]),
+                    np.max([ax.get_xlim(), ax.get_ylim()]),
+                ]
             ax.plot(lims, lims, 'k-', alpha=0.75, zorder=0)
             ax.set_aspect('equal')
             ax.set_xlim(lims)
@@ -72,16 +83,21 @@ def main():
                                 experiment_name,
                                 target_name)
             plots_folder.mkdir(exist_ok=True, parents=True)
-            metadata = {
-                "Title": f"Predictions for {target_name} "
-                         f"for experiment {experiment_name}",
-                "Keywords": "2D materials, machine learning, graph neural network, MEGNet"}
-            try:
-                metadata["Author"] = getpass.getuser()
-            except:
-                pass
-            fig.savefig(Path(plots_folder, f"{this_trial_name}.pdf"),
-                        metadata=metadata)
+            if args.filetype == "pdf":
+                dpi = None
+                metadata = {
+                    "Title": f"Predictions for {target_name} "
+                            f"for experiment {experiment_name}",
+                    "Keywords": "2D materials, machine learning, graph neural network, MEGNet"}
+                try:
+                    metadata["Author"] = getpass.getuser()
+                except:
+                    pass
+            else:
+                dpi = 300
+                metadata = None
+            fig.savefig(Path(plots_folder, f"{this_trial_name}.{args.filetype}"),
+                        dpi=dpi, bbox_inches='tight', metadata=metadata)
 
 if __name__ == "__main__":
     main()
