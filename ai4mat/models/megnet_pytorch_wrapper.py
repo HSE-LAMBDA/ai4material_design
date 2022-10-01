9b07ae417f3db699b90110e9ee0521847ba6f15b
IlyaAgrelov <49361481+IlyaAgrelov@users.noreply.github.com>
2022-10-01 12:37:31 +0300

    Add files via upload

--- ai4mat/models/megnet_pytorch_wrapper.py
+++ ai4mat/models/megnet_pytorch_wrapper.py
@@ -1,7 +1,4 @@
-from lib2to3.pytree import convert
-from typing import Union
 import pandas as pd
-from ai4mat.models.megnet_pytorch.megnet_pytorch_trainer import MEGNetPyTorchTrainer
 import os
 
 
@@ -11,22 +8,22 @@ def set_y(structure, y):
 
 
 def get_megnet_pytorch_predictions(
-        train_structures: Union[pd.Series, pd.DataFrame] ,  # series of pymatgen object
-        train_targets: Union[pd.Series, pd.DataFrame],  # series of scalars
-        test_structures: Union[pd.Series, pd.DataFrame],  # series of pymatgen object
-        test_targets: Union[pd.Series, pd.DataFrame],  # series of scalars
+        train_structures: pd.Series,  # series of pymatgen object
+        train_targets: pd.Series,  # series of scalars
+        test_structures: pd.Series,  # series of pymatgen object
+        test_targets: pd.Series,  # series of scalars
         target_is_intensive: bool,
         model_params: dict,
         gpu: int,
         checkpoint_path,
-        n_jobs,
         ):
     if not target_is_intensive:
         raise NotImplementedError
     os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu)
-    target_name = train_targets.name
+    target_name = model_params['model']['target_name']
     train_targets = train_targets.tolist()
     test_targets = test_targets.tolist()
+
     train_data = [set_y(s, y) for s, y in zip(train_structures, train_targets)]
     test_data = [set_y(s, y) for s, y in zip(test_structures, test_targets)]
 
@@ -37,9 +34,9 @@ def get_megnet_pytorch_predictions(
         configs=model_params,
         gpu_id=gpu,
         save_checkpoint=False,
-        n_jobs=n_jobs
     )
     model.train()
 
     print('========== predicting ==============')
-    return model.predict_test_structures()
+    #return model.predict_test_structures()
+    return model
