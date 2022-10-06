20bba2ee363c05e39bad16c518b9634e9f73b7d6
ruskinkot1 <ruskinkot1@gmail.com>
2022-10-06 15:04:16 +0000

    Revert "Added matminer features+catboost experiments"
    This reverts commit ee8a1146284559223aa025cfd746ff58efe899a3.

--- ai4mat/models/catboost_wrapper.py
+++ ai4mat/models/catboost_wrapper.py
@@ -6,10 +6,9 @@ def get_catboost_predictions(
     x_train, y_train, x_test, y_test, target_is_intensive, model_params, gpu
 ):
     model = CatBoostRegressor(
-        task_type="CPU", bootstrap_type="Bernoulli", devices=str(gpu), **model_params
+        task_type="GPU", bootstrap_type="Bernoulli", devices=str(gpu), **model_params
     )
     model.fit(x_train, y_train)
     predictions = model.predict(x_test)
-    predictions=predictions.reshape(-1, 1)
-    predictions = pd.DataFrame(predictions, index=x_test.index)
+    predictions = pd.Series(predictions, index=x_test.index)
     return predictions
