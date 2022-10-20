e0e210e0a9e4e79801eba0d880de8df6d9a55ad2
RomanovIgnat <romanov.ignat.p@gmail.com>
2022-10-20 13:22:47 +0300

    debug

--- scripts/prepare_data_split.py
+++ scripts/prepare_data_split.py
@@ -1,15 +1,18 @@
 import argparse
 import yaml
 from itertools import combinations
+from functools import reduce
 import pandas as pd
 import numpy as np
 import sys
+
 sys.path.append('.')
 
 from ai4mat.data.data import (
     read_structures_descriptions,
     StorageResolver,
     TEST_FOLD,
+    VALIDATION_FOLD,
 )
 
 
@@ -34,6 +37,16 @@ def get_train_test_split(length, test_size, random_state):
     return np.random.permutation(res)
 
 
+def get_train_val_test_split(length, val_size, test_size, random_state):
+    np.random.seed(random_state)
+    val_length = int(length * val_size)
+    test_length = int(length * test_size)
+    res = np.zeros(length, dtype=int)
+    res[:val_length] = VALIDATION_FOLD
+    res[val_length: val_length + test_length] = TEST_FOLD
+    return np.random.permutation(res)
+
+
 def main():
     parser = argparse.ArgumentParser("Prepares CV data splits")
     parser.add_argument("--datasets", nargs="+", required=True)
@@ -47,30 +60,36 @@ def main():
     parser.add_argument("--drop-na", action="store_true",
                         help="Drop the ids for which fields are missing")
     parser.add_argument("--strategy", type=str, default="cv")
-
-    folds = None
     args = parser.parse_args()
     storage_resolver = StorageResolver()
-    structures = [read_structures_descriptions(storage_resolver["csv_cif"]/dataset_name)
-                  for dataset_name in args.datasets]
 
-    if any(map(indices_intersect, combinations(structures, 2))):
+    all_structures = [read_structures_descriptions(storage_resolver["csv_cif"] / dataset_name)
+                      for dataset_name in args.datasets]
+    sum_weight = reduce(lambda x, y: x * y, [len(i) for i in all_structures])
+    all_weights = [np.ones(len(i)) * sum_weight / len(i) for i in all_structures]
+
+    if any(map(indices_intersect, combinations(all_structures, 2))):
         raise ValueError("Structures contain duplicate indices")
 
     if args.strategy == "train_test":
-        if len(args.datasets) != len(args.test_sizes):
-            raise f"please provide test sizes for all datasets"
-        folds = np.concatenate([
-            get_train_test_split(structures[i].shape[0], args.test_sizes[i], args.random_seed) for i in range(len(structures))
-        ], axis=None)
+        if args.test_size > 0:
+            folds = np.concatenate([
+                get_train_val_test_split(all_structures[i].shape[0], args.test_size, args.val_size, args.random_seed) for i in
+                range(len(all_structures))
+            ], axis=None)
 
-    structures = pd.concat(structures, axis=0)
+        all_structures = pd.concat(all_structures, axis=0)
+        print(all_structures)
+        return
+        all_weights = pd.concat(all_weights, axis=0)
+        if args.drop_na:
+            all_structures.dropna(inplace=True)
 
-    if args.drop_na:
-        structures.dropna(inplace=True)
+        output_path = StorageResolver()["experiments"].joinpath(args.experiment_name)
+        output_path.mkdir(exist_ok=True)
 
     random_state = np.random.RandomState(args.random_seed)
-    
+
     output_path = StorageResolver()["experiments"].joinpath(args.experiment_name)
     output_path.mkdir(exist_ok=True)
     folds = folds if folds is not None else get_folds(len(structures), args.n_folds, random_state)
