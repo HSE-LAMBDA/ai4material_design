29ebd885fbcf4be60cf24b068dfe5d94dfd6d5bb
Abdalaziz <abdalaziz.rashid@outlook.com>
2022-10-27 18:56:46 +0300

    Add unscaled losses

--- ai4mat/models/loss_functions.py
+++ ai4mat/models/loss_functions.py
@@ -1,5 +1,28 @@
 import torch
 
+def MSELoss(y, preds, weights, reduction):
+    '''
+        MSE loss
+    '''
+    if reduction == 'mean':
+        return ((y - preds) ** 2).mean()
+    elif reduction == 'sum':
+        return ((y - preds) ** 2).sum()
+    else:
+        raise ValueError
+
+
+def MAELoss(y, preds, weights, reduction):
+    '''
+        MAE loss
+    '''
+    if reduction == 'mean':
+        return torch.abs(y - preds).mean()
+    elif reduction == 'sum':
+        return torch.abs(y - preds).sum()
+    else:
+        raise ValueError
+
 
 def weightedMSELoss(y, preds, weights, reduction):
     '''
