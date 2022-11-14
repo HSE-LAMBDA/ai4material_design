8cd8e5fb47c96a169544a1f51848c3e238674414
Abdalaziz <abdalaziz.rashid@outlook.com>
2022-11-15 01:16:47 +0300

    Update GemNet

--- ai4mat/models/gemnet/gemnet_trainer.py
+++ ai4mat/models/gemnet/gemnet_trainer.py
@@ -1,21 +1,127 @@
 import os
+from ast import Dict
+
 import numpy as np
 import torch
 
 torch.multiprocessing.set_start_method('forkserver', force=True)
 torch.multiprocessing.set_sharing_strategy('file_system')
-from tqdm import trange
+import logging
+import math
+
+from tqdm import tqdm, trange
 
 import wandb
+from ai4mat.common.base_trainer import Trainer
 from ai4mat.common.config import Config
 from ai4mat.common.ema import ExponentialMovingAverage
-from ai4mat.data.gemnet_dataloader import GemNetFullStruct, GemNetFullStructFolds
-from ai4mat.common.base_trainer import Trainer
+from ai4mat.data.gemnet_dataloader import (GemNetFullStruct,
+                                           GemNetFullStructFolds)
 from ai4mat.models.gemnet.gemnet import GemNetT
-from torch.utils.data import DataLoader
+
+from ..modules.scaling import ScaleFactor
+from ..modules.scaling.util import ensure_fitted
 
 
-class GemNetTrainer(Trainer):
+class FitScalingMixin:
+    def fit_scaling(self):
+        try:
+            ensure_fitted(self.model)
+        except ValueError:
+            self.scales()
+        
+    def scales(self):
+        logging.info("Model not fitted yet")
+        data_batch = next(iter(self.trainloader)).cuda()
+        out = self.model(data_batch)
+        _ = torch.nn.functional.l1_loss(out.view(-1), getattr(data_batch, 'metadata'))
+        del out, _
+
+        scale_factors = {
+        name: module
+            for name, module in self.model.named_modules()
+        if isinstance(module, ScaleFactor)
+        }
+        fitted_scale_factors = [
+            f"{name}: {module.scale_factor.item():.3f}"
+            for name, module in scale_factors.items()
+            if module.fitted
+        ]
+        fitted_scale_factors_str = ", ".join(fitted_scale_factors)
+        logging.info(f"Fitted scale factors: [{fitted_scale_factors_str}]")
+        unfitted_scale_factors = [
+            name for name, module in scale_factors.items() if not module.fitted
+        ]
+        unfitted_scale_factors_str = ", ".join(unfitted_scale_factors)
+        logging.info(f"Unfitted scale factors: [{unfitted_scale_factors_str}]")
+        
+
+        for name, scale_factor in scale_factors.items():
+            if scale_factor.fitted:
+                logging.info(
+                    f"{name} is already fitted in the checkpoint, resetting it. {scale_factor.scale_factor}"
+                )
+            scale_factor.reset_()
+
+
+        scale_factor_indices: Dict[str, int] = {}
+        max_idx = 0
+
+        # initialize all scale factors
+        for name, module in scale_factors.items():
+
+            def index_fn(name=name):
+                nonlocal max_idx
+                assert name is not None
+                if name not in scale_factor_indices:
+                    scale_factor_indices[name] = max_idx
+                    logging.debug(f"Scale factor for {name} = {max_idx}")
+                    max_idx += 1
+
+            module.initialize_(index_fn=index_fn)
+
+        # single pass through network
+        _ = self.model(data_batch)
+        del _
+
+        # sort the scale factors by their computation order
+        sorted_factors = sorted(
+            scale_factors.items(),
+            key=lambda x: scale_factor_indices.get(x[0], math.inf),
+        )
+
+        logging.info("Sorted scale factors by computation order:")
+        for name, _ in sorted_factors:
+            logging.info(f"{name}: {scale_factor_indices[name]}")
+
+        # endregion
+
+        # loop over the scale factors in the computation order
+        # and fit them one by one
+        logging.info("Start fitting")
+
+        for name, module in sorted_factors:
+            logging.info(f"Fitting {name}...")
+            with module.fit_context_():
+                for batch in tqdm(iter(self.trainloader)):
+                    out = self.model(batch.cuda())
+                    _ = torch.nn.functional.l1_loss(out.view(-1), getattr(batch, 'metadata'))
+                    del out, _
+                stats, ratio, value = module.fit_()
+
+                logging.info(
+                    f"Variable: {name}, "
+                    f"Var_in: {stats['variance_in']:.3f}, "
+                    f"Var_out: {stats['variance_out']:.3f}, "
+                    f"Ratio: {ratio:.3f} => Scaling factor: {value:.3f}"
+                )
+
+        # make sure all scale factors are fitted
+        for name, module in sorted_factors:
+            assert module.fitted, f"{name} is not fitted"
+
+
+class GemNetTrainer(Trainer, FitScalingMixin):
     def __init__(self, 
         train_structures=None,
         train_targets=None,
@@ -88,10 +194,10 @@ class GemNetTrainer(Trainer):
         self.ema = ExponentialMovingAverage(
             self.model.parameters(), self.config["optim"]["ema_decay"]
         )
-
+        self.fit_scaling()
 
     def train(self):
-
+        ensure_fitted(self.model)
         # Define the custom x axis metric
         wandb.define_metric("epoch")
         wandb.define_metric("dataloader_step")
@@ -139,6 +245,7 @@ class GemNetTrainer(Trainer):
             )
 
     def predict_structures(self, structures):
+        ensure_fitted(self.model)
         # data_list = self.structures.prepare(self.structures.get_ase_atoms(structures), targets=None)
         data_list = self.structures.construct_dataset(structures, targets=None)
         results = []
