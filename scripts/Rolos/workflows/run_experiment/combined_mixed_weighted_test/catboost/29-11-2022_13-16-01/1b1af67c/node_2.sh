84c14c335b5c685149e772208f1d4ee9d10a102f
Nikita Kazeev <kazeevn@gmail.com>
2023-03-21 16:35:06 +0800

    \n to space

--- scripts/Rolos/workflows/run_experiment/combined_mixed_weighted_test/catboost/29-11-2022_13-16-01/1b1af67c/node_2.sh
+++ scripts/Rolos/workflows/run_experiment/combined_mixed_weighted_test/catboost/29-11-2022_13-16-01/1b1af67c/node_2.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd ai4material_design
 export WANDB_API_KEY=ae457f48d5eb86299f2fe9c18497a281b029a295
-parallel -j 2 python run_experiments.py --gpus 0 --processes-per-unit 1 --wandb-entity hse_lambda --targets homo_lumo_gap_min --experiments combined_mixed_weighted_test --trials ::: stability/catboost/29-11-2022_13-16-01/1b1af67c/3
-stability/catboost/29-11-2022_13-16-01/1b1af67c/4
+parallel -j 2 python run_experiments.py --gpus 0 --processes-per-unit 1 --wandb-entity hse_lambda --targets homo_lumo_gap_min --experiments combined_mixed_weighted_test --trials ::: stability/catboost/29-11-2022_13-16-01/1b1af67c/3 stability/catboost/29-11-2022_13-16-01/1b1af67c/4
--- scripts/Rolos/workflows/run_experiment/combined_mixed_weighted_test/megnet_pytorch/sparse/05-12-2022_19-50-53/d6b7ce45/node_1.sh
+++ scripts/Rolos/workflows/run_experiment/combined_mixed_weighted_test/megnet_pytorch/sparse/05-12-2022_19-50-53/d6b7ce45/node_1.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 cd ai4material_design
 export WANDB_API_KEY=ae457f48d5eb86299f2fe9c18497a281b029a295
-parallel -j 4 python run_experiments.py --gpus 0 --processes-per-unit 1 --wandb-entity hse_lambda --targets formation_energy_per_site --experiments combined_mixed_weighted_test --trials ::: stability/megnet_pytorch/sparse/05-12-2022_19-50-53/d6b7ce45/1
-stability/megnet_pytorch/sparse/05-12-2022_19-50-53/d6b7ce45/2
-stability/megnet_pytorch/sparse/05-12-2022_19-50-53/d6b7ce45/3
-stability/megnet_pytorch/sparse/05-12-2022_19-50-53/d6b7ce45/4
+parallel -j 4 python run_experiments.py --gpus 0 --processes-per-unit 1 --wandb-entity hse_lambda --targets formation_energy_per_site --experiments combined_mixed_weighted_test --trials ::: stability/megnet_pytorch/sparse/05-12-2022_19-50-53/d6b7ce45/1 stability/megnet_pytorch/sparse/05-12-2022_19-50-53/d6b7ce45/2 stability/megnet_pytorch/sparse/05-12-2022_19-50-53/d6b7ce45/3 stability/megnet_pytorch/sparse/05-12-2022_19-50-53/d6b7ce45/4
