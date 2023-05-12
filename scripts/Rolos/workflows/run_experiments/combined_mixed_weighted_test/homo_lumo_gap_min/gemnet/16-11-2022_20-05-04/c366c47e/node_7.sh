c06733cc6666a277fc383d41365c71a58bb7609a
Nikita Kazeev <kazeevn@gmail.com>
2023-05-12 11:51:55 +0800

    WanDB config into workflow scripts

--- scripts/Rolos/workflows/run_experiments/combined_mixed_weighted_test/homo_lumo_gap_min/gemnet/16-11-2022_20-05-04/c366c47e/node_7.sh
+++ scripts/Rolos/workflows/run_experiments/combined_mixed_weighted_test/homo_lumo_gap_min/gemnet/16-11-2022_20-05-04/c366c47e/node_7.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ai4material_design
-export WANDB_API_KEY=ae457f48d5eb86299f2fe9c18497a281b029a295
+source scripts/Rolos/wandb_config.sh
 python run_experiments.py --n-jobs 8 --output-folder /output --gpus 0 --processes-per-unit 1 --wandb-entity hse_lambda --targets homo_lumo_gap_min --experiments combined_mixed_weighted_test --trials stability/gemnet/16-11-2022_20-05-04/c366c47e/7
