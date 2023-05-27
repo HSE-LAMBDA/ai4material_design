befdace42bd3dba0a71f77da4b34fc03ae0fea35
Abdalaziz Rashid <abdalaziz.rashid@outlook.com>
2023-05-27 23:26:05 +0000

    Specify root_folder argument

--- scripts/compute_matminer_features.py
+++ scripts/compute_matminer_features.py
@@ -46,8 +46,7 @@ def main():
             )
         )
     features_df = pd.DataFrame(features, index=structures.index)
-
-    save_dir = StorageResolver(args.output_folder)["processed"].joinpath(dataset_name)
+    save_dir = StorageResolver(root_folder=args.output_folder)["processed"].joinpath(dataset_name)
     save_dir.mkdir(exist_ok=True)
     if args.debug:
         file_name = "matminer_dbg.csv.gz"
